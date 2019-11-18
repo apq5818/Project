94ffa8115da429f6fdaf1466c28f7fa45356835f
apq5818 <apq5818@PF18R5NE.springernature.com>
2019-11-18 15:48:41 +0530

    initial commit

---
+++ JBookbucket/src/main/java/com/sn/JBookbucketApplication.java
@@ -0,0 +1,13 @@
+package com.sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBookbucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBookbucketApplication.class, args);
+	}
+
+}
