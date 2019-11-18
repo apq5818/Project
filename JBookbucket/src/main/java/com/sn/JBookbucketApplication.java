8fb81b3fead4bc0216b49afed5e6125aea8ab8ab
apq5818 <apq5818@PF18R5NE.springernature.com>
2019-11-18 15:52:12 +0530

    Testing

--- JBookbucket/src/main/java/com/sn/JBookbucketApplication.java
+++ JBookbucket/src/main/java/com/sn/JBookbucketApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JBookbucketApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(JBookbucketApplication.class, args);
